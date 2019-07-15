a99efdee03bc4884b864cb662c5d9559a020e638
toannguyentrung <toan.nguyentrung@gmail.com>
2019-07-15 22:00:23 +0700

    This first commit

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var customer = GetCustomers().FirstOrDefault(c => c.Id == Id);
+
+            return View(customer);
+        }
+
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "John Smith"},
+                new Customer {Id = 2, Name = "Jane Elishzabeth"}
+            };
+        }
+    } 
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page 2.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page 1.";
+
+
+
+            return View();
+
+        }
+
+
+
+    }
+}
